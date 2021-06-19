64cf27892fde9d1db7552f8b4f0f70bee34fcefe
ADefWebserver <Webmaster@adefwebserver.com>
2021-06-19 13:57:54 -0700

    Solution builds (does not work)

---
+++ ADefHelpDeskApp/Data/Models/LogsPaged.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ADefHelpDeskApp.Data.Models
+{
+    public partial class LogsPaged
+    {
+        public List<Logs> Logs { get; set; }
+        public int LogCount { get; set; }
+    }
+}
--- ADefHelpDeskWebApp/Startup.cs
+++ ADefHelpDeskWebApp/Startup.cs
@@ -1,5 +1,3 @@
-using ADefHelpDeskWebApp.Areas.Identity;
-using ADefHelpDeskWebApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -13,16 +11,66 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Loader;
 using System.Threading.Tasks;
+using WilderMinds.MetaWeblog;
 
 namespace ADefHelpDeskWebApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(env.ContentRootPath)
+           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+           .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            // Before we load the CustomClassLibrary.dll (and potentially lock it)
+            // Determine if we have files in the Upgrade directory and process it first
+            if (System.IO.File.Exists(env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.dll"))
+            {
+                string WebConfigOrginalFileNameAndPath = env.ContentRootPath + @"\Web.config";
+                string WebConfigTempFileNameAndPath = env.ContentRootPath + @"\Web.config.txt";
+
+                if (System.IO.File.Exists(WebConfigOrginalFileNameAndPath))
+                {
+                    // Temporarily rename the web.config file
+                    // to release the locks on any assemblies
+                    System.IO.File.Copy(WebConfigOrginalFileNameAndPath, WebConfigTempFileNameAndPath);
+                    System.IO.File.Delete(WebConfigOrginalFileNameAndPath);
+
+                    // Give the site time to release locks on the assemblies
+                    Task.Delay(2000).Wait(); // Wait 2 seconds with blocking
+
+                    // Rename the temp web.config file back to web.config
+                    // so the site will be active again
+                    System.IO.File.Copy(WebConfigTempFileNameAndPath, WebConfigOrginalFileNameAndPath);
+                    System.IO.File.Delete(WebConfigTempFileNameAndPath);
+                }
+
+                // Delete current 
+                System.IO.File.Delete(env.ContentRootPath + @"\CustomModules\ADefHelpDeskApp.dll");
+                //System.IO.File.Delete(env.ContentRootPath + @"\CustomModules\ADefHelpDeskApp.Views.dll");
+
+                // Copy new 
+                System.IO.File.Copy(
+                    env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.dll",
+                    env.ContentRootPath + @"\CustomModules\ADefHelpDeskApp.dll");
+                //System.IO.File.Copy(
+                //    env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.Views.dll",
+                //    env.ContentRootPath + @"\CustomModules\ADefHelpDeskApp.Views.dll");
+
+                // Delete Upgrade - so it wont be processed again
+                System.IO.File.Delete(env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.dll");
+                //System.IO.File.Delete(env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.Views.dll");
+            }
         }
 
         public IConfiguration Configuration { get; }
@@ -31,16 +79,28 @@ namespace ADefHelpDeskWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddSingleton<WeatherForecastService>();
+            var ADefHelpDeskAppViewsPath = Path.GetFullPath(@"CustomModules\ADefHelpDeskApp.Views.dll");
+
+            var ADefHelpDeskAppViewsAssembly =
+                AssemblyLoadContext
+                .Default.LoadFromAssemblyPath(ADefHelpDeskAppViewsPath);
+
+            var ADefHelpDeskAppPath = Path.GetFullPath(@"CustomModules\ADefHelpDeskApp.dll");
+
+            var ADefHelpDeskAppAssembly =
+                AssemblyLoadContext
+                .Default.LoadFromAssemblyPath(ADefHelpDeskAppPath);
+
+            Type ADefHelpDeskAppType =
+                ADefHelpDeskAppAssembly
+                .GetType("Microsoft.Extensions.DependencyInjection.RegisterServices");
+
+            services.AddMvc(options => options.EnableEndpointRouting = false)
+                .AddApplicationPart(ADefHelpDeskAppViewsAssembly)
+                .AddApplicationPart(ADefHelpDeskAppAssembly);
+
+            ADefHelpDeskAppType.GetMethod("AddADefHelpDeskAppServices")
+                .Invoke(null, new object[] { services, Configuration });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,7 +114,9 @@ namespace ADefHelpDeskWebApp
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days.
+                // You may want to change this for production scenarios,
+                // see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -65,6 +127,8 @@ namespace ADefHelpDeskWebApp
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseMetaWeblog("/MetaWeblog");
 
             app.UseEndpoints(endpoints =>
             {
