c1c9f1d8f2a12115dd8fc00867d9a5668a6d1cf9
Michael Washington <webmaster@adefwebserver.com>
2021-06-20 17:22:21 -0700

    Project builds and starts up

--- ADefHelpDeskApp/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
+++ ADefHelpDeskApp/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
@@ -5,10 +9,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System;
-using System.Security.Claims;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace ADefHelpDeskApp.Areas.Identity
 {
--- ADefHelpDeskWebApp/Program.cs
+++ ADefHelpDeskWebApp/Program.cs
@@ -20,6 +20,7 @@ namespace ADefHelpDeskWebApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseStaticWebAssets();
                     webBuilder.UseStartup<Startup>();
                 });
     }
