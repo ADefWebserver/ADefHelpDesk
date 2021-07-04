9b3a856d59b9dac15511386efa37798963ea2a81
Michael Washington <webmaster@adefwebserver.com>
2021-07-04 09:13:25 -0700

    Create Admin works

--- ADefHelpDeskApp/Models/DataContext/ApplicationDbContext.cs
+++ ADefHelpDeskApp/Models/DataContext/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AdefHelpDeskBase.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,7 +7,7 @@ using System.Text;
 
 namespace ADefHelpDeskApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
