7a67ba6dce5000716b2cf1b3e39592f7a7fab5aa
Michael Washington <webmaster@adefwebserver.com>
2021-08-06 17:48:43 -0700

    Application Settings in progress

---
+++ ADefHelpDeskApp/Classes/SelectItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADefHelpDeskApp.Classes
+{
+    public class SelectItem
+    {
+        public string ItemLabel { get; set; }
+        public string ItemValue { get; set; }
+    }
+}
