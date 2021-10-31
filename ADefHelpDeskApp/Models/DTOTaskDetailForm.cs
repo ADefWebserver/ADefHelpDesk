1e5caf3822e1bc7fe2e241c238e809ebe2ee9c96
Michael Washington <webmaster@adefwebserver.com>
2021-10-31 16:54:20 -0700

    Form layout of Create Ticket

---
+++ ADefHelpDeskApp/Models/DTOTaskDetailForm.cs
@@ -0,0 +1,42 @@
+﻿//
+// ADefHelpDesk.com
+// Copyright (c) 2021
+// by Michael Washington
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+//
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdefHelpDeskBase.Models
+{
+    public class DTOTaskDetailForm
+    {
+        [Key]
+        public int DetailId { get; set; }
+        public string DetailType { get; set; }
+        public string ContentType { get; set; }
+        public DateTime InsertDate { get; set; }
+        public int UserId { get; set; }
+        public string Description { get; set; }
+        public string EmailDescription { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? StopTime { get; set; }
+
+        public List<DTOAttachment> colDTOAttachment { get; set; }
+    }
+}
