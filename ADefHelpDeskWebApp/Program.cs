c8dc370ccea06a5285d3e21be8a13ebe676e3282
Michael Washington <webmaster@adefwebserver.com>
2023-01-07 13:43:24 -0800

    Remove build warnings

--- ADefHelpDeskWebApp/Program.cs
+++ ADefHelpDeskWebApp/Program.cs
@@ -6,96 +6,94 @@ using System.Runtime.Loader;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+// Read the connection string from the appsettings.json file
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+// Get HostingEnvironment
+var env = builder.Environment;
+
+// Before we load the CustomClassLibrary.dll (and potentially lock it)
+// Determine if we have files in the Upgrade directory and process it first
+if (System.IO.File.Exists(env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.dll"))
 {
-    var env = hostingContext.HostingEnvironment;
+    string WebConfigOrginalFileNameAndPath = env.ContentRootPath + @"\Web.config";
+    string WebConfigTempFileNameAndPath = env.ContentRootPath + @"\Web.config.txt";
 
-    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-    config.AddEnvironmentVariables();
-
-    // Before we load the CustomClassLibrary.dll (and potentially lock it)
-    // Determine if we have files in the Upgrade directory and process it first
-    if (System.IO.File.Exists(env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.dll"))
+    if (System.IO.File.Exists(WebConfigOrginalFileNameAndPath))
     {
-        string WebConfigOrginalFileNameAndPath = env.ContentRootPath + @"\Web.config";
-        string WebConfigTempFileNameAndPath = env.ContentRootPath + @"\Web.config.txt";
+        // Temporarily rename the web.config file
+        // to release the locks on any assemblies
+        System.IO.File.Copy(WebConfigOrginalFileNameAndPath, WebConfigTempFileNameAndPath);
+        System.IO.File.Delete(WebConfigOrginalFileNameAndPath);
 
-        if (System.IO.File.Exists(WebConfigOrginalFileNameAndPath))
-        {
-            // Temporarily rename the web.config file
-            // to release the locks on any assemblies
-            System.IO.File.Copy(WebConfigOrginalFileNameAndPath, WebConfigTempFileNameAndPath);
-            System.IO.File.Delete(WebConfigOrginalFileNameAndPath);
+        // Give the site time to release locks on the assemblies
+        Task.Delay(2000).Wait(); // Wait 2 seconds with blocking
 
-            // Give the site time to release locks on the assemblies
-            Task.Delay(2000).Wait(); // Wait 2 seconds with blocking
-
-            // Rename the temp web.config file back to web.config
-            // so the site will be active again
-            System.IO.File.Copy(WebConfigTempFileNameAndPath, WebConfigOrginalFileNameAndPath);
-            System.IO.File.Delete(WebConfigTempFileNameAndPath);
-        }
-
-        // Delete current 
-        System.IO.File.Delete(env.ContentRootPath + @"\CustomModules\ADefHelpDeskApp.dll");
-
-        // Copy new 
-        System.IO.File.Copy(
-            env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.dll",
-            env.ContentRootPath + @"\CustomModules\ADefHelpDeskApp.dll");
-
-        // Delete Upgrade - so it wont be processed again
-        System.IO.File.Delete(env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.dll");
+        // Rename the temp web.config file back to web.config
+        // so the site will be active again
+        System.IO.File.Copy(WebConfigTempFileNameAndPath, WebConfigOrginalFileNameAndPath);
+        System.IO.File.Delete(WebConfigTempFileNameAndPath);
     }
 
-    var ADefHelpDeskAppPath = Path.GetFullPath(@"CustomModules\ADefHelpDeskApp.dll");
+    // Delete current 
+    System.IO.File.Delete(env.ContentRootPath + @"\CustomModules\ADefHelpDeskApp.dll");
 
-    var ADefHelpDeskAppAssembly =
-        AssemblyLoadContext
-        .Default.LoadFromAssemblyPath(ADefHelpDeskAppPath);
+    // Copy new 
+    System.IO.File.Copy(
+        env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.dll",
+        env.ContentRootPath + @"\CustomModules\ADefHelpDeskApp.dll");
 
-    Type ADefHelpDeskAppType =
-        ADefHelpDeskAppAssembly
-        .GetType("Microsoft.Extensions.DependencyInjection.RegisterServices");
+    // Delete Upgrade - so it wont be processed again
+    System.IO.File.Delete(env.ContentRootPath + @"\Upgrade\ADefHelpDeskApp.dll");
+}
 
-    // AddApplicationPart is what makes the WebAPI routes
-    // from the external assembly availiable
-    builder.Services.AddMvc(options => options.EnableEndpointRouting = false)
-            .AddApplicationPart(ADefHelpDeskAppAssembly);
+var ADefHelpDeskAppPath = Path.GetFullPath(@"CustomModules\ADefHelpDeskApp.dll");
 
-    ADefHelpDeskAppType.GetMethod("AddADefHelpDeskAppServices")
-        .Invoke(null, new object[] { builder });
+var ADefHelpDeskAppAssembly =
+    AssemblyLoadContext
+    .Default.LoadFromAssemblyPath(ADefHelpDeskAppPath);
 
-    var app = builder.Build();
+Type ADefHelpDeskAppType =
+    ADefHelpDeskAppAssembly
+    .GetType("Microsoft.Extensions.DependencyInjection.RegisterServices");
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseMigrationsEndPoint();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
+// AddApplicationPart is what makes the WebAPI routes
+// from the external assembly availiable
+builder.Services.AddMvc(options => options.EnableEndpointRouting = false)
+        .AddApplicationPart(ADefHelpDeskAppAssembly);
 
-    app.UseHttpsRedirection();
+ADefHelpDeskAppType.GetMethod("AddADefHelpDeskAppServices")
+    .Invoke(null, new object[] { builder });
 
-    app.UseStaticFiles();
+var app = builder.Build();
 
-    app.UseRouting();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-    app.UseAuthentication();
-    app.UseAuthorization();
+app.UseHttpsRedirection();
 
-    app.MapControllers();
-    app.MapBlazorHub();
-    app.MapFallbackToPage("/_Host");
+app.UseStaticFiles();
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseRouting();
 
-    app.Run();
-});
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Run();
