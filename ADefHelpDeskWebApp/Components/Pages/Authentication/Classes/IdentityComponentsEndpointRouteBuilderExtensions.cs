ab3ef0d5ca7405f70adb377a9436a0dd1d82380c
Michael Washington <webmaster@adefwebserver.com>
2024-07-26 18:55:26 -0700

    Remove /DownloadPersonalData

--- ADefHelpDeskWebApp/Components/Pages/Authentication/Classes/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ ADefHelpDeskWebApp/Components/Pages/Authentication/Classes/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -51,46 +51,7 @@ namespace Microsoft.AspNetCore.Routing
                 return TypedResults.LocalRedirect($"~/{returnUrl}");
             }).WithMetadata(new HideFromSwaggerAttribute());
 
-            var manageGroup = accountGroup.MapGroup("/Manage").RequireAuthorization();
-
-            var loggerFactory = endpoints.ServiceProvider.GetRequiredService<ILoggerFactory>();
-            var downloadLogger = loggerFactory.CreateLogger("DownloadPersonalData");
-
-            manageGroup.MapPost("/DownloadPersonalData", async (
-                HttpContext context,
-                [FromServices] UserManager<ApplicationUser> userManager,
-                [FromServices] AuthenticationStateProvider authenticationStateProvider) =>
-            {
-                var user = await userManager.GetUserAsync(context.User);
-                if (user is null)
-                {
-                    return Results.NotFound($"Unable to load user with ID '{userManager.GetUserId(context.User)}'.");
-                }
-
-                var userId = await userManager.GetUserIdAsync(user);
-                downloadLogger.LogInformation("User with ID '{UserId}' asked for their personal data.", userId);
-
-                // Only include personal data for download
-                var personalData = new Dictionary<string, string>();
-                var personalDataProps = typeof(ApplicationUser).GetProperties().Where(
-                    prop => Attribute.IsDefined(prop, typeof(PersonalDataAttribute)));
-                foreach (var p in personalDataProps)
-                {
-                    personalData.Add(p.Name, p.GetValue(user)?.ToString() ?? "null");
-                }
-
-                var logins = await userManager.GetLoginsAsync(user);
-                foreach (var l in logins)
-                {
-                    personalData.Add($"{l.LoginProvider} external login provider key", l.ProviderKey);
-                }
-
-                personalData.Add("Authenticator Key", (await userManager.GetAuthenticatorKeyAsync(user))!);
-                var fileBytes = JsonSerializer.SerializeToUtf8Bytes(personalData);
-
-                context.Response.Headers.TryAdd("Content-Disposition", "attachment; filename=PersonalData.json");
-                return TypedResults.File(fileBytes, contentType: "application/json", fileDownloadName: "PersonalData.json");
-            }).WithMetadata(new HideFromSwaggerAttribute());
+            var manageGroup = accountGroup.MapGroup("/Manage").RequireAuthorization();            
 
             return accountGroup;
         }
